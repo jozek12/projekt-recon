2c5bc56f6618fdcbd73ce9f5069ca21e52dd068d
miszkas <mishka12@o2.pl>
2025-06-13 16:06:46 +0200

    added user preferences generator

--- db_scripts/user_simulated/add_users.py
+++ db_scripts/user_simulated/add_users.py
@@ -3,7 +3,7 @@ from random_username.generate import generate_username
 
 conn = connection.get_connection("recon.db")
 c = conn.cursor()
-how_much = 20
+how_much = 10000
 for _ in range(how_much):
     username = generate_username(1)
     c.execute("INSERT INTO Users (name) VALUES (?)", (username))
